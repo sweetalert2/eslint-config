2b33ba26fea47ef241e8c7e66fae3e81feae8af3
Limon Monte <limon.monte@gmail.com>
2022-01-17 21:06:14 +0200

    fix: play well together with prettier

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/no-this-alias': 0,
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/no-empty-interface': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-unused-vars': 'error',
+    'brace-style': 0,
     'comma-dangle': 0,
     'complexity': ['error', { max: 10 }],
     'import/named': 'error',
@@ -26,7 +26,5 @@ module.exports = {
     'no-use-before-define': 0,
     'no-var': ['error'],
     'prefer-template': 'error',
-    'quote-props': ['error', 'consistent'],
-    'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
   }
 }
