064cb198ccf75a72c5434395839a7b2a5de3055f
Limon Monte <limonte@users.noreply.github.com>
2022-01-23 00:17:18 +0200

    fix: remove some rules

--- index.js
+++ index.js
@@ -11,17 +11,12 @@ module.exports = {
     'prettier',
   ],
   rules: {
-    '@typescript-eslint/no-this-alias': 0,
     '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-empty-interface': 0,
-    '@typescript-eslint/no-explicit-any': 0,
     'complexity': ['error', { max: 10 }],
     'no-console': [
       'error',
       { allow: ['warn', 'error', 'info'] },
     ],
-    'no-use-before-define': 0,
-    'no-var': ['error'],
     'prefer-template': 'error',
   },
 }
