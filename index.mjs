7c4c0749b48bc6c1f36eae087281043e23862d32
Limon Monte <6059356+limonte@users.noreply.github.com>
2024-08-01 17:22:16 +0300

    fix: use es module for config format

---
+++ index.mjs
@@ -0,0 +1,27 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.js', '**/*.ts'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports' },
+      ],
+      'no-console': [
+        'error',
+        { allow: ['warn', 'error', 'info'] },
+      ],
+      'prefer-template': 'error',
+      'sort-imports': [
+        'error',
+        { ignoreDeclarationSort: true },
+      ],
+    },
+  }
+)
